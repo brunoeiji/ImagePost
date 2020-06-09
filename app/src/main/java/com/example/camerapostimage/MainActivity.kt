80a5efb2c93206447c6688ad1bfc61cb32223502
Bruno Eiji <brunoeijioy@gmail.com>
2020-06-09 12:51:02 -0300

    added url input and send button

--- app/src/main/java/com/example/camerapostimage/MainActivity.kt
+++ app/src/main/java/com/example/camerapostimage/MainActivity.kt
@@ -12,28 +12,41 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import java.util.Base64
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.extensions.jsonBody
+import com.github.kittinunf.result.Result
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.android.synthetic.main.activity_main.view.*
 import java.io.ByteArrayOutputStream
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     private val CAMERA_REQUEST = 0x42
     private val GALLERY_REQUEST = 0x41
     private val PERMISSION_CODE = 0x40
-    private val POST_URL = "http://172.18.0.1:8080/detect" //COMPLETAR AQUI
+
+    private val httpChoices = arrayOf("HTTP", "HTTPS")
+    private var selectedImage: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, httpChoices)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        httpSpinner.adapter = arrayAdapter
+        httpSpinner.onItemSelectedListener = this
+
+        urlTextField.editText?.setText(getURL())
 
         cameraButton.setOnClickListener {
             openCamera()
@@ -41,6 +54,10 @@ class MainActivity : AppCompatActivity() {
 
         galleryButton.setOnClickListener {
             checkPermissionForGallery()
+        }
+
+        sendButton.setOnClickListener {
+            sendImage()
         }
     }
 
@@ -58,6 +75,33 @@ class MainActivity : AppCompatActivity() {
         }
         else{ //system OS is < Marshmallow
             openGallery();
+        }
+    }
+
+    private fun createURL() : String{
+        val url = httpSpinner.selectedItem.toString().toLowerCase()+ "://" + urlTextField.editText?.text
+        Log.e("URL",url)
+        return url
+    }
+
+    private fun sendImage(){
+        if(selectedImage != null) {
+            if(urlTextField.editText?.text?.isNotEmpty()!!) {
+                progressBar.visibility = View.VISIBLE
+                uploadImageToKasco(selectedImage!!)
+            }else{
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Error")
+                builder.setMessage("Coloque uma URL para enviar a imagem")
+                builder.setPositiveButton("Ok", null)
+                builder.show()
+            }
+        }else{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Error")
+            builder.setMessage("Escolha uma imagem para enviar")
+            builder.setPositiveButton("Ok", null)
+            builder.show()
         }
     }
 
@@ -80,30 +124,39 @@ class MainActivity : AppCompatActivity() {
 
     private fun uploadImageToKasco(image:Bitmap){
         getBase64Image(image, complete = { base64Image ->
-            GlobalScope.launch {
-                val body = "{ \"image\":\"" + base64Image + "\"}"
-                Log.d("BASE 64 IMAGE", body)
-                Fuel.post(POST_URL)
-                    .jsonBody(body)
-                    .response { result ->
-                        Log.e("result", "response chegou")
-                        val toast = Toast.makeText(applicationContext, "Resposta", Toast.LENGTH_LONG)
-                        toast.show()
+            val body = "{ \"image\":\"" + base64Image + "\"}"
+//                Log.d("BASE 64 IMAGE", body)
+            val url = createURL()
+            saveURL(url)
+            Fuel.post(url)
+                .jsonBody(body)
+                .responseString { result ->
+                    progressBar.visibility = View.INVISIBLE
+                    when (result) {
+                        is Result.Failure -> {
+                            val er = result.getException()
+                            Log.e("Error", er.toString())
+                            runOnUiThread { Toast.makeText(this@MainActivity, "Falhou", Toast.LENGTH_LONG).show() }
+                        }
+                        is Result.Success -> {
+                            val data = result.get()
+                            println(data)
+                            Log.e("Data", data)
+                            runOnUiThread { Toast.makeText(this@MainActivity, "Sucesso", Toast.LENGTH_LONG).show() }
+                        }
                     }
-            }
+                }
         })
     }
 
     private fun getBase64Image(image: Bitmap, complete: (String?) -> Unit){
-        GlobalScope.launch {
-            val outputStream = ByteArrayOutputStream()
-            image.compress(Bitmap.CompressFormat.PNG, 100, outputStream )
-            val b = outputStream.toByteArray()
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                complete(Base64.getEncoder().encodeToString(b))
-            }else{
-                complete(null)
-            }
+        val outputStream = ByteArrayOutputStream()
+        image.compress(Bitmap.CompressFormat.PNG, 100, outputStream )
+        val b = outputStream.toByteArray()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            complete(Base64.getEncoder().encodeToString(b))
+        }else{
+            complete(null)
         }
     }
 
@@ -120,6 +173,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun saveURL(url: String){
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE)
+        with(sharedPref.edit()){
+            putString("url", url)
+            apply()
+        }
+    }
+
+    private fun getURL() : String{
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE)
+        return sharedPref.getString("url", "")!!
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -131,7 +197,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        var selectedImage: Bitmap? = null
+
         if(requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK){
             selectedImage = data?.extras?.get("data") as Bitmap
         }else if(requestCode == GALLERY_REQUEST && resultCode == Activity.RESULT_OK) {
@@ -145,19 +211,15 @@ class MainActivity : AppCompatActivity() {
             super.onActivityResult(requestCode, resultCode, data)
         }
         if(selectedImage != null) {
-            uploadImageToKasco(selectedImage)
             imageView.setImageBitmap(selectedImage)
-        }else{
-            val alertDialog: AlertDialog? = this.let {
-                val builder = AlertDialog.Builder(it)
-                builder.setTitle("Error")
-                builder.setMessage("Bitmap null, não foi possível fazer o upload")
-                builder.apply {
-                    setPositiveButton("Ok", null)
-                }
-
-                builder.create()
-            }
         }
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        p0?.textView?.text =  httpChoices[0]
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        p0?.textView?.text =  httpChoices[p2]
     }
 }
